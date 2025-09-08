927ac5d69395880282fecb85406494aec5eec408
Kmmoonlight <1501898512@qq.com>
2025-09-08 15:32:46 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,9 @@ import { buttonVariants } from "@/components/ui/button"
 export default function Home() {
   return (
     <section className="flex min-h-[86.5vh] flex-col items-center justify-center px-2 py-8 text-center">
-      <h1 className="mb-4 text-4xl font-bold sm:text-7xl">Documents</h1>
+      <h1 className="mb-4 text-4xl font-bold sm:text-7xl">Flutter Widgets</h1>
       <p className="text-foreground mb-8 max-w-[600px] sm:text-base">
-        A simple open-source product documentation platform. That&apos;s simple
-        to use and easy to customize.
+        关于Flutter Widget的文档罗列，方便进行随时查看。
       </p>
 
       <div className="flex items-center gap-5">
