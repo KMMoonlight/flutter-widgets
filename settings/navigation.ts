6603c4ab6831baf834e6a18c21072ad2c6a97c05
Kmmoonlight <1501898512@qq.com>
2025-09-08 14:41:33 +0800

    Update navigation.ts

--- settings/navigation.ts
+++ settings/navigation.ts
@@ -4,14 +4,9 @@ export const Navigations = [
   {
     title: "Docs",
     href: `/docs${PageRoutes[0].href}`,
-  },
-  {
-    title: "Home",
-    href: "https://rubixstudios.com.au",
-    external: true,
-  },
+  }
 ]
 
 export const GitHubLink = {
-  href: "https://github.com/rubixvi/rubix-documents",
+  href: "",
 }
