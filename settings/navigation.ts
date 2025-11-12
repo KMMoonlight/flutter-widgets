934dd782a604f06a932df9ebcda3db031f253fc6
Kmmoonlight <1501898512@qq.com>
2025-11-12 15:57:42 +0800

    Update navigation.ts

--- settings/navigation.ts
+++ settings/navigation.ts
@@ -5,6 +5,16 @@ export const Navigations = [
     title: "Docs",
     href: `/docs${PageRoutes[0].href}`,
     external: false
+  },
+  {
+    title: "基础组件",
+    href: `/docs/basic-widgets`,
+    external: false
+  },
+  {
+    title: "设计系统",
+    href: `/docs/design-system`,
+    external: false
   }
 ]
 
